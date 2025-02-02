8c6880f3d384a0195b41f174d61ebadc74cb31f3
unknown <prandoz@gmail.com>
2025-02-02 13:24:56 +0100

    Init refactor

---
+++ src/server/IdroAlertER.Application/Entities/ValoriStazione.cs
@@ -0,0 +1,14 @@
+﻿namespace IdroAlertER.Application.Entities;
+internal class ValoriStazione
+{
+	public string? IdStazione { get; set; }
+	public int? Ordinamento { get; set; }
+	public string? NomeStaz { get; set; }
+	public string? Lon { get; set; }
+	public string? Lat { get; set; }
+	public double? ValoreAttuale { get; set; }
+	public double? ValorePrecedente { get; set; }
+	public double? SogliaGialla { get; set; }
+	public double? SogliaArancione { get; set; }
+	public double? SogliaRossa { get; set; }
+}
--- src/server/IdroAlertER.Application/Services/LivelloIdrometricoBLService.cs
+++ src/server/IdroAlertER.Application/Services/LivelloIdrometricoBLService.cs
@@ -1,108 +1,93 @@
-﻿using IdroAlertER.Common.Interfaces.Repositories;
+﻿using System.Text;
+using IdroAlertER.Application.Entities;
+using IdroAlertER.Common.Entities.Results;
+using IdroAlertER.Common.Interfaces.Repositories;
 using IdroAlertER.Common.Interfaces.Services;
 using Microsoft.Extensions.Configuration;
 
 namespace IdroAlertER.Application.Services;
 internal class LivelloIdrometricoBLService : ILivelloIdrometricoBLService
 {
-	private readonly IConfiguration _configuration;
 	private readonly ITimeStampService _timeStampService;
 	private readonly ILivelloIdrometricoHttpService _livelloIdrometricoHttpService;
+	private readonly long _timeStampAttuale;
+	private readonly List<string> _nomiStazioni;
+	private List<IdroAlertHttpResult>? _valoriStazioniAttuali;
+	private List<IdroAlertHttpResult>? _valoriStazioniPrecedenti;
+	private List<IdroAlertHttpResult>? _valoriStazioni;
 
 	public LivelloIdrometricoBLService(IConfiguration configuration, ITimeStampService timeStampService, ILivelloIdrometricoHttpService livelloIdrometricoHttpService)
 	{
-		_configuration = configuration;
 		_timeStampService = timeStampService;
 		_livelloIdrometricoHttpService = livelloIdrometricoHttpService;
+		_timeStampAttuale = timeStampService.Get();
+		_nomiStazioni = configuration.GetSection("NomiStazioni").Get<List<string>>() ?? new();
 	}
 
 	public async Task ExecuteAsync()
 	{
-		var timeStampAttuale = _timeStampService.Get();
-		var valoriStazioniAttuali = await _livelloIdrometricoHttpService.GetAsync(timeStampAttuale);
+		_valoriStazioniAttuali = await _livelloIdrometricoHttpService.GetAsync(_timeStampAttuale);
 		// TODO: rimuovere
-		valoriStazioniAttuali = await _livelloIdrometricoHttpService.GetAsync(1738437300000);
-		var valoriStazioniPrecedente = await _livelloIdrometricoHttpService.GetAsync(_timeStampService.GetBefore(timeStampAttuale));
+		_valoriStazioniAttuali = await _livelloIdrometricoHttpService.GetAsync(1738437300000);
+		_valoriStazioniPrecedenti = await _livelloIdrometricoHttpService.GetAsync(_timeStampService.GetBefore(_timeStampAttuale));
 
-		foreach (var nomeStazione in _configuration.GetSection("NomiStazioni").Get<List<string>>())
+		foreach (var nomeStazione in _nomiStazioni)
 		{
-			var valoreStazioneAttuale = valoriStazioniAttuali.FirstOrDefault(vsa => vsa.NomeStaz == nomeStazione);
-			var valoreStazionePrecedente = valoriStazioniPrecedente.FirstOrDefault(vsp => vsp.NomeStaz == nomeStazione);
+			var message = new StringBuilder();
+			var valoreStazioneAttuale = valoriStazioniAttuali?.FirstOrDefault(vsa => vsa.NomeStaz == nomeStazione) ?? new IdroAlertHttpResult();
+			var valoreStazionePrecedente = valoriStazioniPrecedente?.FirstOrDefault(vsp => vsp.NomeStaz == nomeStazione) ?? new IdroAlertHttpResult();
 
 			// Se è cambiato valore dalla lettura precedente
 			if (valoreStazioneAttuale.Value != valoreStazionePrecedente.Value)
 			{
+				message.AppendFormat("Il livello della stazione {0} ", valoreStazioneAttuale.NomeStaz);
+				var differenzaLivello = Math.Round((decimal)(valoreStazioneAttuale.Value > valoreStazionePrecedente.Value
+										? valoreStazioneAttuale.Value - valoreStazionePrecedente.Value : valoreStazionePrecedente.Value - valoreStazioneAttuale.Value), 2);
+
 				// Se il valore è aumentato
 				if (valoreStazioneAttuale.Value >= valoreStazioneAttuale.Value)
 				{
 					if (valoreStazioneAttuale.Value >= valoreStazioneAttuale.Soglia1)
 					{
-						if (valoreStazionePrecedente.Value < valoreStazionePrecedente.Soglia1)
+						if (valoreStazioneAttuale.Value >= valoreStazioneAttuale.Soglia2)
 						{
-							Alert();
-						}
-						else
-						{
-							if (valoreStazioneAttuale.Value >= valoreStazioneAttuale.Soglia2)
+							if (valoreStazioneAttuale.Value >= valoreStazioneAttuale.Soglia3)
 							{
-								if (valoreStazioneAttuale.Value >= valoreStazioneAttuale.Soglia3)
+								if (valoreStazionePrecedente.Value < valoreStazionePrecedente.Soglia3)
 								{
-									// Se è cambiata la soglia
-									if (valoreStazionePrecedente.Value < valoreStazionePrecedente.Soglia3)
-									{
-										Alert();
-									}
-								}
-								else
-								{
-									if (valoreStazionePrecedente.Value < valoreStazionePrecedente.Soglia2)
-									{
-										Alert();
-									}
+									message.Append("è passato in soglia ROSSA ed è aumentato di {0} metri", differenzaLivello);
 								}
 							}
 							else
 							{
-								if (valoreStazionePrecedente.Value < valoreStazionePrecedente.Soglia1)
-								{
-									Alert();
-								}
+								// Sopra soglia 2
 							}
 						}
+						else
+						{
+							// Sopra Soglia 1
+						}
+					}
+					else
+					{
+						// Sotto Soglia1
 					}
 				}
 				else // Se il valore è diminuito
 				{
-					if (valoreStazioneAttuale.Value < valoreStazioneAttuale.Soglia3)
-					{
-						if (valoreStazioneAttuale.Value < valoreStazioneAttuale.Soglia2)
-						{
-							if (valoreStazioneAttuale.Value < valoreStazioneAttuale.Soglia1)
-							{
-								// Se è cambiata la soglia
-								if (valoreStazionePrecedente.Value >= valoreStazionePrecedente.Soglia1)
-								{
-									Alert();
-								}
-							}
-							else
-							{
-								if (valoreStazionePrecedente.Value >= valoreStazionePrecedente.Soglia2)
-								{
-									Alert();
-								}
-							}
-						}
-						else
-						{
-							if (valoreStazionePrecedente.Value >= valoreStazionePrecedente.Soglia3)
-							{
-								Alert();
-							}
-						}
-					}
+
 				}
 			}
+		}
+	}
+
+	private ValoriStazione GetValoreStazione(string nomeStazione)
+	{
+		var valoriStazione = _valoriStazioniAttuali?.FirstOrDefault(vsa => vsa.NomeStaz == nomeStazione);
+
+		if (valoriStazione == null || valoriStazione.Value == null)
+		{
+
 		}
 	}
 
